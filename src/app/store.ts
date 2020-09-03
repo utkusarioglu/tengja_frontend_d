4fd65bc18d9b8c3a2e09b53ba88341a43f6691a6
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-09-03 17:25:40 +0300

    Add basic websocket support

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,16 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import reduxWebsocket from '@giantmachines/redux-websocket';
+import playerReducer from '../features/player/playerSlice';
 // import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
+    player: playerReducer,
     // counter: counterReducer,
   },
+  middleware: [
+    reduxWebsocket(),
+  ]
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { connect, send } from '@giantmachines/redux-websocket';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,6 +16,13 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+const WEBSOCKET_URL = `ws://localhost:4000/ws`
+store.dispatch(connect(WEBSOCKET_URL));
+setTimeout(() => {
+  console.log(`now sending websocket message to ${WEBSOCKET_URL}`)
+  store.dispatch(send("yellow hellow"));
+}, 2000)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
