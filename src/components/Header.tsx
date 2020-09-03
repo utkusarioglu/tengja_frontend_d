06cf497c52f0c530bb0d9977a3f34e42f81c62c5
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-09-03 13:56:15 +0300

    Add svg logo to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
+import logo from '../../svg/logo.svg';
 
-function Header() {
+const styles: {[className: string]: CSSProperties} = {
+    logo: {
+        width: '20vw',
+    }
+}
+
+interface OwnProps {
+    pageName: string;
+}
+
+type Props = OwnProps;
+
+function Header(props: Props) {
     return (
         <div>
-            <h2>Tengja</h2>
-            <p>Page Name</p>
+            <img
+              src={logo}
+              style={styles.logo}
+              alt="website logo"
+            />
+            <p>{props.pageName}</p>
 
         </div>
     )
