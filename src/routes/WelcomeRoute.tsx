dd401eadac1cc02ef3ec71c3325692847cd47a0b
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-09-04 11:36:15 +0300

    Add isConnected state check to WelcomeRoute

--- src/routes/WelcomeRoute.tsx
+++ src/routes/WelcomeRoute.tsx
@@ -1,6 +1,8 @@
 import React, { CSSProperties } from 'react';
 import {Link} from "react-router-dom";
 import logo from '../svg/logo.svg';
+import { useSelector } from 'react-redux';
+import { selectIsConnected } from '../features/app/appSlice';
 
 const styles: {[className: string]: CSSProperties} = {
     logo: {
@@ -10,6 +12,8 @@ const styles: {[className: string]: CSSProperties} = {
 }
 
 function Welcome() {
+    const isConnected = useSelector(selectIsConnected);
+
     return (
         <>
             <img
@@ -17,14 +21,23 @@ function Welcome() {
               style={styles.logo}
               alt="website logo"
             />
-            <div>
-                <Link to="/play/player-create">
-                    <button>Play</button>
-                </Link>
-                <Link to="/watch">
-                    <button>Watch</button>
-                </Link>
-            </div>
+            {
+                isConnected 
+                ? (
+                    <div>
+                        <Link to="/play/player-create">
+                            <button>Play</button>
+                        </Link>
+                        <Link to="/watch">
+                            <button>Watch</button>
+                        </Link>
+                    </div>
+
+                )
+                :  (
+                    <p>Connecting</p>
+                )
+            }
         </>
     )
 }
